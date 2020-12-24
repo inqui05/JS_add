d777b0212bacf057312b7157a2910b1b69439cf5
Tratsiuk Artsiom <inqui05l@gmail.com>
2020-12-24 13:34:42 +0300

    my additional  homework to lesson18

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+const input = document.querySelector('.input'),
+    text = document.querySelector('#text');
+
+
+function debounce(func, time) {
+    let timeout,
+        immediate = false;
+
+    return function executedFunction() {
+        // Сохраняем контекст this и переданные параметры
+        const context = this;
+        const args = arguments;
+
+        // Функция, вызываемая по истечению времени debounce.
+        const later = function() {
+            timeout = null;
+
+            // Вызываем функцию, если immediate !== true,
+            if (!immediate) func.apply(context, args);
+        };
+
+        // clearTimeout сбрасывает ожидание при каждом выполнении функции.
+        clearTimeout(timeout);
+
+        // Перезапускаем период ожидания debounce.
+        timeout = setTimeout(later, time);
+    };
+}
+
+const logger = debounce(() => {
+    text.textContent = input.value;
+}, 1000);
+
+input.addEventListener('keyup', logger);
